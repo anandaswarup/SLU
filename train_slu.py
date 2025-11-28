a4ed9c7968ab2394b99d00e72360f518dad478e4
anandaswarup <anandaswarup.vadapalli@gmail.com>
2025-11-28 15:12:20 +0530

    Training script for Spoken Language Understanding (SLU) models

--- train_slu.py
+++ train_slu.py
@@ -1,7 +1,15 @@
 """
-Script for training a HuBERT Encoder -> Attention GRU Decoder SLU model with NLL loss.
+Training script for Spoken Language Understanding (SLU) models.
 
-Usage: python train_hubert_encoder_gru_decoder_slu.py --hparams hparams/hubert_encoder_gru_decoder_slu.yaml
+Supports multiple SLU architectures:
+- CRDNN ASR Encoder (pre-trained on LibriSpeech) -> LSTM Encoder -> Attention GRU Decoder
+- HuBERT Encoder -> Attention GRU Decoder
+
+The model architecture is determined by the hyperparameters YAML file.
+
+Usage:
+    python train_slu.py --hparams hparams/crdnn_librispeech_encoder_seq2seq_slu.yaml
+    python train_slu.py --hparams hparams/hubert_encoder_gru_decoder_slu.yaml
 """
 
 import argparse
@@ -17,39 +25,15 @@ from prepare import prepare_SLU_dataset
 
 logger = get_logger(__name__)
 
+# Global variables set from args
+show_results_every = 100
+tokenizer = None
 
-class SLU(sb.Brain):
-    def compute_forward(self, batch, stage):
-        """
-        Forward computations from the waveform batches to the output probabilities.
-        """
-        batch = batch.to(self.device)
-        wavs, wav_lens = batch.sig
-        tokens_bos, tokens_bos_lens = batch.tokens_bos
 
-        # Add waveform augmentation if specified.
-        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "wav_augment"):
-            wavs, wav_lens = self.hparams.wav_augment(wavs, wav_lens)
-            tokens_bos = self.hparams.wav_augment.replicate_labels(tokens_bos)
-
-        #  encoder forward pass
-        wav2vec2_out = self.modules.wav2vec2(wavs, wav_lens)  # type: ignore
-
-        # SLU forward pass
-        e_in = self.hparams.output_emb(tokens_bos)
-        h, _ = self.hparams.dec(e_in, wav2vec2_out, wav_lens)
-
-        # Output layer for seq2seq log-probabilities
-        logits = self.hparams.seq_lin(h)
-        p_seq = self.hparams.log_softmax(logits)
-
-        # Compute outputs
-        if stage == sb.Stage.TRAIN and self.step % show_results_every != 0:
-            return p_seq, wav_lens
-        else:
-            hyps, _, _, _ = self.hparams.beam_searcher(wav2vec2_out.detach(), wav_lens)
-
-            return p_seq, wav_lens, hyps
+class BaseSLU(sb.Brain):
+    """
+    Base class for SLU models with common functionality.
+    """
 
     def compute_objectives(self, predictions, batch, stage):
         """
@@ -70,7 +54,6 @@ class SLU(sb.Brain):
             tokens_eos_lens = self.hparams.wav_augment.replicate_labels(tokens_eos_lens)
 
         loss_seq = self.hparams.seq_cost(p_seq, tokens_eos, length=tokens_eos_lens)
-
         loss = loss_seq
 
         if (
@@ -78,7 +61,8 @@ class SLU(sb.Brain):
         ) and predicted_tokens is not None:
             # Decode token terms to words
             predicted_semantics = [
-                tokenizer.decode_ids(utt_seq).split(" ") for utt_seq in predicted_tokens
+                tokenizer.decode_ids(utt_seq).split(" ")  # type: ignore
+                for utt_seq in predicted_tokens
             ]
 
             target_semantics = [wrd.split(" ") for wrd in batch.semantics]
@@ -96,16 +80,15 @@ class SLU(sb.Brain):
 
     def on_stage_start(self, stage, epoch):  # type: ignore
         """
-        Gets called at the beginning of each epoch
+        Gets called at the beginning of each epoch.
         """
-
         if stage != sb.Stage.TRAIN:
             self.cer_metric = self.hparams.cer_computer()
             self.wer_metric = self.hparams.error_rate_computer()
 
     def on_stage_end(self, stage, stage_loss, epoch):  # type: ignore
         """
-        Gets called at the end of a epoch.
+        Gets called at the end of an epoch.
         """
         # Compute/store important stats
         stage_stats = {"loss": stage_loss}
@@ -117,28 +100,7 @@ class SLU(sb.Brain):
 
         # Perform end-of-iteration things, like annealing, logging, etc.
         if stage == sb.Stage.VALID:
-            old_lr, new_lr = self.hparams.lr_annealing(stage_stats["WER"])
-            (
-                old_lr_wav2vec2,
-                new_lr_wav2vec2,
-            ) = self.hparams.lr_annealing_wav2vec2(stage_stats["WER"])
-            sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)  # type: ignore
-            sb.nnet.schedulers.update_learning_rate(  # type: ignore
-                self.wav2vec2_optimizer, new_lr_wav2vec2
-            )
-            self.hparams.train_logger.log_stats(
-                stats_meta={
-                    "epoch": epoch,
-                    "lr": old_lr,
-                    "wave2vec_lr": old_lr_wav2vec2,
-                },
-                train_stats=self.train_stats,
-                valid_stats=stage_stats,
-            )
-            self.checkpointer.save_and_keep_only(  # type: ignore
-                meta={"WER": stage_stats["WER"]},
-                min_keys=["WER"],
-            )
+            self._on_valid_stage_end(stage_stats, epoch)
         elif stage == sb.Stage.TEST:
             self.hparams.train_logger.log_stats(
                 stats_meta={"Epoch loaded": self.hparams.epoch_counter.current},
@@ -148,31 +110,162 @@ class SLU(sb.Brain):
                 with open(self.hparams.test_wer_file, "w", encoding="utf-8") as w:
                     self.wer_metric.write_stats(w)
 
+    def _on_valid_stage_end(self, stage_stats, epoch):
+        """
+        Handle validation stage end. Override in subclasses for custom LR scheduling.
+        """
+        old_lr, new_lr = self.hparams.lr_annealing(stage_stats["WER"])
+        sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)  # type: ignore
+        self.hparams.train_logger.log_stats(
+            stats_meta={"epoch": epoch, "lr": old_lr},
+            train_stats=self.train_stats,
+            valid_stats=stage_stats,
+        )
+        self.checkpointer.save_and_keep_only(  # type: ignore
+            meta={"WER": stage_stats["WER"]},
+            min_keys=["WER"],
+        )
+
+
+class CRDNNSLU(BaseSLU):
+    """
+    SLU model using pre-trained CRDNN ASR Encoder -> LSTM Encoder -> Attention GRU Decoder.
+    """
+
+    def compute_forward(self, batch, stage):
+        """
+        Forward computations from the waveform batches to the output probabilities.
+        """
+        batch = batch.to(self.device)
+        wavs, wav_lens = batch.sig
+        tokens_bos, tokens_bos_lens = batch.tokens_bos
+
+        # Add waveform augmentation if specified.
+        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "wav_augment"):
+            orig_device = wavs.device
+            wavs_cpu = wavs.to("cpu")
+            wav_lens_cpu = wav_lens.to("cpu")
+
+            with torch.no_grad():
+                aug_wavs_cpu, aug_wav_lens_cpu = self.hparams.wav_augment(
+                    wavs_cpu, wav_lens_cpu
+                )
+
+            wavs = aug_wavs_cpu.to(orig_device)
+            wav_lens = aug_wav_lens_cpu.to(orig_device)
+
+            tokens_bos = self.hparams.wav_augment.replicate_labels(tokens_bos)
+            tokens_bos_lens = self.hparams.wav_augment.replicate_labels(tokens_bos_lens)
+
+        # ASR encoder forward pass
+        with torch.no_grad():
+            ASR_encoder_out = self.hparams.asr_model.encode_batch(
+                wavs.detach(), wav_lens
+            )
+
+        # SLU forward pass
+        encoder_out = self.hparams.slu_enc(ASR_encoder_out)
+        e_in = self.hparams.output_emb(tokens_bos)
+        h, _ = self.hparams.dec(e_in, encoder_out, wav_lens)
+
+        # Output layer for seq2seq log-probabilities
+        logits = self.hparams.seq_lin(h)
+        p_seq = self.hparams.log_softmax(logits)
+
+        # Compute outputs
+        if stage == sb.Stage.TRAIN and self.step % show_results_every != 0:
+            return p_seq, wav_lens
+        else:
+            p_tokens, _, _, _ = self.hparams.beam_searcher(encoder_out, wav_lens)
+            return p_seq, wav_lens, p_tokens
+
+
+class HuBERTSLU(BaseSLU):
+    """
+    SLU model using HuBERT Encoder -> Attention GRU Decoder.
+    """
+
+    def compute_forward(self, batch, stage):
+        """
+        Forward computations from the waveform batches to the output probabilities.
+        """
+        batch = batch.to(self.device)
+        wavs, wav_lens = batch.sig
+        tokens_bos, tokens_bos_lens = batch.tokens_bos
+
+        # Add waveform augmentation if specified.
+        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "wav_augment"):
+            wavs, wav_lens = self.hparams.wav_augment(wavs, wav_lens)
+            tokens_bos = self.hparams.wav_augment.replicate_labels(tokens_bos)
+
+        # Encoder forward pass
+        hubert_out = self.modules.hubert(wavs, wav_lens)  # type: ignore
+
+        # SLU forward pass
+        e_in = self.hparams.output_emb(tokens_bos)
+        h, _ = self.hparams.dec(e_in, hubert_out, wav_lens)
+
+        # Output layer for seq2seq log-probabilities
+        logits = self.hparams.seq_lin(h)
+        p_seq = self.hparams.log_softmax(logits)
+
+        # Compute outputs
+        if stage == sb.Stage.TRAIN and self.step % show_results_every != 0:
+            return p_seq, wav_lens
+        else:
+            hyps, _, _, _ = self.hparams.beam_searcher(hubert_out.detach(), wav_lens)
+            return p_seq, wav_lens, hyps
+
+    def _on_valid_stage_end(self, stage_stats, epoch):
+        """
+        Handle validation stage end with dual LR schedulers for HuBERT.
+        """
+        old_lr, new_lr = self.hparams.lr_annealing(stage_stats["WER"])
+        old_lr_hubert, new_lr_hubert = self.hparams.lr_annealing_hubert(
+            stage_stats["WER"]
+        )
+        sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)  # type: ignore
+        sb.nnet.schedulers.update_learning_rate(  # type: ignore
+            self.hubert_optimizer, new_lr_hubert
+        )
+        self.hparams.train_logger.log_stats(
+            stats_meta={
+                "epoch": epoch,
+                "lr": old_lr,
+                "hubert_lr": old_lr_hubert,
+            },
+            train_stats=self.train_stats,
+            valid_stats=stage_stats,
+        )
+        self.checkpointer.save_and_keep_only(  # type: ignore
+            meta={"WER": stage_stats["WER"]},
+            min_keys=["WER"],
+        )
+
     def init_optimizers(self):
         """
-        Initializes the wav2vec2 optimizer and model optimizer
+        Initializes the HuBERT optimizer and model optimizer.
         """
-        self.wav2vec2_optimizer = self.hparams.wav2vec2_opt_class(
-            self.modules.wav2vec2.parameters()  # type: ignore
+        self.hubert_optimizer = self.hparams.hubert_opt_class(
+            self.modules.hubert.parameters()  # type: ignore
         )
         self.optimizer = self.hparams.opt_class(self.hparams.model.parameters())
 
         if self.checkpointer is not None:
-            self.checkpointer.add_recoverable("wav2vec2_opt", self.wav2vec2_optimizer)
+            self.checkpointer.add_recoverable("hubert_opt", self.hubert_optimizer)
             self.checkpointer.add_recoverable("optimizer", self.optimizer)
 
         self.optimizers_dict = {
-            "wav2vec_optimizer": self.wav2vec2_optimizer,
+            "hubert_optimizer": self.hubert_optimizer,
             "model_optimizer": self.optimizer,
         }
 
 
 def dataio_prepare(hparams):
     """
-    This function prepares the datasets to be used in the brain class.
-    It also defines the data processing pipeline through user-defined functions.
+    Prepares the datasets to be used in the brain class.
+    Defines the data processing pipeline through user-defined functions.
     """
-
     data_folder = hparams["data_folder"]
 
     train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(  # type: ignore
@@ -181,19 +274,13 @@ def dataio_prepare(hparams):
     )
 
     if hparams["sorting"] == "ascending":
-        # we sort training data to speed up training and get better results.
         train_data = train_data.filtered_sorted(sort_key="duration")
-        # when sorting do not shuffle in dataloader ! otherwise is pointless
         hparams["dataloader_opts"]["shuffle"] = False
-
     elif hparams["sorting"] == "descending":
         train_data = train_data.filtered_sorted(sort_key="duration", reverse=True)
-        # when sorting do not shuffle in dataloader ! otherwise is pointless
         hparams["dataloader_opts"]["shuffle"] = False
-
     elif hparams["sorting"] == "random":
         pass
-
     else:
         raise NotImplementedError("sorting must be random, ascending or descending")
 
@@ -211,9 +298,9 @@ def dataio_prepare(hparams):
 
     datasets = [train_data, valid_data, test_data]
 
-    tokenizer = hparams["tokenizer"]
+    tok = hparams["tokenizer"]
 
-    # 2. Define audio pipeline:
+    # Define audio pipeline
     @sb.utils.data_pipeline.takes("wav")  # type: ignore
     @sb.utils.data_pipeline.provides("sig")  # type: ignore
     def audio_pipeline(wav):
@@ -222,16 +309,16 @@ def dataio_prepare(hparams):
 
     sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)  # type: ignore
 
-    # 3. Define text pipeline:
+    # Define text pipeline
     @sb.utils.data_pipeline.takes("semantics")  # type: ignore
     @sb.utils.data_pipeline.provides(  # type: ignore
         "semantics", "token_list", "tokens_bos", "tokens_eos", "tokens"
     )
     def text_pipeline(semantics):
         yield semantics
-        tokens_list = tokenizer.encode_as_ids(semantics)
+        tokens_list = tok.encode_as_ids(semantics)
         yield tokens_list
-        tokens_bos = torch.LongTensor([hparams["bos_index"]] + (tokens_list))
+        tokens_bos = torch.LongTensor([hparams["bos_index"]] + tokens_list)
         yield tokens_bos
         tokens_eos = torch.LongTensor(tokens_list + [hparams["eos_index"]])
         yield tokens_eos
@@ -240,18 +327,69 @@ def dataio_prepare(hparams):
 
     sb.dataio.dataset.add_dynamic_item(datasets, text_pipeline)  # type: ignore
 
-    # 4. Set output:
+    # Set output keys
     sb.dataio.dataset.set_output_keys(  # type: ignore
         datasets,
         ["id", "sig", "semantics", "tokens_bos", "tokens_eos", "tokens"],
     )
-    return train_data, valid_data, test_data, tokenizer
+
+    return train_data, valid_data, test_data, tok
+
+
+def get_model_type(hparams):
+    """
+    Determines the model type based on hyperparameters.
+    """
+    if "asr_model_path" in hparams:
+        return "crdnn"
+    elif "hubert_hub" in hparams or "hubert" in hparams:
+        return "hubert"
+    else:
+        raise ValueError(
+            "Could not determine model type from hyperparameters. "
+            "Expected 'asr_model_path' for CRDNN or 'hubert_hub' for HuBERT."
+        )
+
+
+def setup_crdnn_model(hparams, run_opts):
+    """
+    Sets up the CRDNN-based SLU model.
+    """
+    from speechbrain.inference.ASR import EncoderDecoderASR
+
+    # Prepare augmentation data if available
+    if "prepare_noise_data" in hparams:
+        run_on_main(hparams["prepare_noise_data"])
+    if "prepare_rir_data" in hparams:
+        run_on_main(hparams["prepare_rir_data"])
+
+    # Load the pre-trained ASR model
+    hparams["asr_model"] = EncoderDecoderASR.from_hparams(
+        source=hparams["asr_model_path"],
+        run_opts={"device": run_opts["device"]},
+    )
+
+    return CRDNNSLU
+
+
+def setup_hubert_model(hparams, run_opts):
+    """
+    Sets up the HuBERT-based SLU model.
+    """
+    # Move HuBERT to device
+    hparams["hubert"] = hparams["hubert"].to(run_opts["device"])
+
+    # Freeze the feature extractor part when unfreezing
+    if not hparams["freeze_hubert"] and hparams["freeze_hubert_conv"]:
+        hparams["hubert"].model.feature_extractor._freeze_parameters()
+
+    return HuBERTSLU
 
 
 def parse_args():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Train a HuBERT Encoder -> Attention GRU Decoder SLU model"
+        description="Train a Spoken Language Understanding (SLU) model"
     )
     parser.add_argument(
         "--hparams",
@@ -296,7 +434,9 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
+    global show_results_every, tokenizer
+
     # Parse command-line arguments
     args = parse_args()
 
@@ -314,7 +454,7 @@ if __name__ == "__main__":
 
     show_results_every = args.show_results_every
 
-    # create ddp_group with the right communication protocol
+    # Create ddp_group with the right communication protocol
     ddp_init_group(run_opts)
 
     # Create experiment directory for logging artifacts
@@ -328,7 +468,7 @@ if __name__ == "__main__":
     if script_copy.exists():
         script_copy.unlink()
 
-    # multi-gpu (ddp) save data preparation
+    # Prepare SLU dataset
     run_on_main(
         prepare_SLU_dataset,
         kwargs={
@@ -338,22 +478,24 @@ if __name__ == "__main__":
         },
     )
 
-    # here we create the datasets objects as well as tokenization and encoding
-    (train_set, valid_set, test_set, tokenizer) = dataio_prepare(hparams)
+    # Prepare datasets and tokenizer
+    train_set, valid_set, test_set, tokenizer = dataio_prepare(hparams)
 
-    # We download and pretrain the tokenizer
+    # Load pretrained tokenizer
     hparams["pretrainer"].collect_files()
     hparams["pretrainer"].load_collected()
 
-    # Move the wav2vec2
-    hparams["wav2vec2"] = hparams["wav2vec2"].to(run_opts["device"])
+    # Determine model type and set up accordingly
+    model_type = get_model_type(hparams)
+    logger.info(f"Detected model type: {model_type}")
 
-    # freeze the feature extractor part when unfreezing
-    if not hparams["freeze_wav2vec2"] and hparams["freeze_wav2vec2_conv"]:
-        hparams["wav2vec2"].model.feature_extractor._freeze_parameters()
+    if model_type == "crdnn":
+        SLUClass = setup_crdnn_model(hparams, run_opts)
+    else:  # hubert
+        SLUClass = setup_hubert_model(hparams, run_opts)
 
     # Brain class initialization
-    slu_brain = SLU(
+    slu_brain = SLUClass(
         modules=hparams["modules"],
         opt_class=hparams["opt_class"],
         hparams=hparams,
@@ -361,7 +503,7 @@ if __name__ == "__main__":
         checkpointer=hparams["checkpointer"],
     )
 
-    # adding objects to trainer:
+    # Add tokenizer to trainer
     slu_brain.tokenizer = tokenizer  # type: ignore
 
     # Training
@@ -375,3 +517,7 @@ if __name__ == "__main__":
 
     # Test
     slu_brain.evaluate(test_set, test_loader_kwargs=hparams["dataloader_opts"])
+
+
+if __name__ == "__main__":
+    main()
