a5f446bb03f3028a77c5f9aa08b33bc2f18b4e25
anandaswarup <anandaswarup.vadapalli@gmail.com>
2025-12-01 18:22:36 +0530

    Bash script to trigger SLU model training

--- train_slu.sh
+++ train_slu.sh
@@ -4,7 +4,7 @@ echo "Activating environment"
 source .venv/bin/activate
 
 echo "SLU model training start"
-python train_slu.py --hparams hparams/hubert_encoder_gru_decoder_slu.yaml
+python python train_slu.py --hparams hparams/distillHuBERT_encoder_gru_decoder_slu.yaml
 echo "SLU model training complete"
 
 echo "Deactivating environment"
